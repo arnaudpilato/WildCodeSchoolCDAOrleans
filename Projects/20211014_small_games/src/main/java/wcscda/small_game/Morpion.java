1b9fd633956c4e2a94012872b8dbd230b690699b
Pil <70975556+arnaudpilato@users.noreply.github.com>
2021-10-14 16:15:17 +0200

    add somethings

--- Projects/20211014_small_games/src/main/java/wcscda/small_game/Morpion.java
+++ Projects/20211014_small_games/src/main/java/wcscda/small_game/Morpion.java
@@ -4,6 +4,10 @@ import java.awt.*;
 import java.awt.image.ImageObserver;
 
 public class Morpion extends Drawable implements SmallGameInterface {
+    private int positionTopX1 = 390;
+    private int positionDownX1 = 390;
+    private int positionTopY1 = 390;
+    private int positionDownY1 = 390;
     @Override
     public void draw(Graphics2D g, ImageObserver io) {
         drawGrid(g, io);
@@ -13,5 +17,13 @@ public class Morpion extends Drawable implements SmallGameInterface {
     }
 
     private void drawGrid(Graphics2D g, ImageObserver io) {
+        g.setColor(Color.green);
+        g.setStroke(new BasicStroke(5));
+        int distance = 0;
+        for (int i = 0; i < 2; i++) {
+            g.drawLine(390, 330 + distance, 810, 330 + distance);
+            g.drawLine(530 + distance, 190, 530 + distance, 610);
+            distance += 140;
+        }
     }
 }
--- Projects/20211014_small_games/src/main/java/wcscda/small_game/MouseController.java
+++ Projects/20211014_small_games/src/main/java/wcscda/small_game/MouseController.java
@@ -5,9 +5,35 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.image.ImageObserver;
+import java.util.ArrayList;
 
 public class MouseController extends MouseAdapter {
+    private static ArrayList<Integer> positionX = new ArrayList<>();
+    private static ArrayList<Integer> positionY = new ArrayList<>();
+    private final int[][] grid = {
+            { 0, 0, 0 },
+            { 0, 0, 0 },
+            { 0, 0, 0 }
+    };
+
     private final Board board;
+
+    public static ArrayList<Integer> getPositionX() {
+        return positionX;
+    }
+
+    public static void setPositionX(ArrayList<Integer> positionX) {
+        MouseController.positionX = positionX;
+    }
+
+    public static ArrayList<Integer> getPositionY() {
+        return positionY;
+    }
+
+    public static void setPositionY(ArrayList<Integer> positionY) {
+        MouseController.positionY = positionY;
+    }
+
 
     public MouseController(Board board) {
         this.board = board;
@@ -18,18 +44,246 @@ public class MouseController extends MouseAdapter {
         board.redraw(new Drawable(){
             @Override
             public void draw(Graphics2D g, ImageObserver io) {
-                int x = mouseEvent.getX();
-                int y = mouseEvent.getY();
 
-                g.setColor(Color.WHITE);
-                g.drawLine(x - 30, y - 30, x + 30, y +30);
-                g.drawLine(x + 30, y - 30, x - 30, y +30);
+                boolean player1 = true;
+
+                /*if (mouseEvent.getX() < 530) {
+                    if (mouseEvent.getY() < 330 && grid[0][0] == 0) {
+                        positionX.add(460);
+                        positionY.add(260);
+                        if (player1) {
+                            grid[0][0] = 1;
+                        } else {
+                            grid[0][0] = 2;
+                        }
+                    }
+
+                    if (mouseEvent.getY() >= 330 && mouseEvent.getY() <= 470 && grid[1][0] == 0) {
+                        positionX.add(460);
+                        positionY.add(400);
+                        if (player1) {
+                            grid[1][0] = 1;
+                        } else {
+                            grid[1][0] = 2;
+                        }
+                    }
+
+                    if (mouseEvent.getY() > 470 && grid[2][0] == 0) {
+                        positionX.add(460);
+                        positionY.add(540);
+                        if (player1) {
+                            grid[2][0] = 1;
+                        } else {
+                            grid[2][0] = 2;
+                        }
+                    }
+                }
+
+                if ((mouseEvent.getX() >= 530) && mouseEvent.getX() <= 670) {
+                    if (mouseEvent.getY() < 330 && grid[0][1] == 0) {
+                        positionX.add(600);
+                        positionY.add(260);
+                        if (player1) {
+                            grid[0][1] = 1;
+                        } else {
+                            grid[0][1] = 2;
+                        }
+                    }
+
+                    if (mouseEvent.getY() >= 330 && mouseEvent.getY() <= 470 && grid[1][1] == 0) {
+                        positionX.add(600);
+                        positionY.add(400);
+                        if (player1) {
+                            grid[1][1] = 1;
+                        } else {
+                            grid[1][1] = 2;
+                        }
+                    }
+
+                    if (mouseEvent.getY() > 470 && grid[2][1] == 0) {
+                        positionX.add(600);
+                        positionY.add(540);
+                        if (player1) {
+                            grid[2][1] = 1;
+                        } else {
+                            grid[2][1] = 2;
+                        }
+                    }
+                }
+
+                if (mouseEvent.getX() >= 670) {
+                    if (mouseEvent.getY() < 330 && grid[0][2] == 0) {
+                        positionX.add(740);
+                        positionY.add(260);
+                        if (player1) {
+                            grid[0][2] = 1;
+                        } else {
+                            grid[0][2] = 2;
+                        }
+                    }
+
+                    if (mouseEvent.getY() >= 330 && mouseEvent.getY() <= 470 && grid[1][2] == 0) {
+                        positionX.add(740);
+                        positionY.add(400);
+                        if (player1) {
+                            grid[1][2] = 1;
+                        } else {
+                            grid[1][2] = 2;
+                        }
+                    }
+
+                    if (mouseEvent.getY() > 470 && grid[2][2] == 0) {
+                        positionX.add(740);
+                        positionY.add(540);
+                        if (player1) {
+                            grid[2][2] = 1;
+                        } else {
+                            grid[2][2] = 2;
+                        }
+                    }
+                } */
+
+                if (mouseEvent.getX() < 530) {
+                    if (mouseEvent.getY() < 330 && grid[0][0] == 0) {
+                        positionX.add(460);
+                        positionY.add(260);
+                        if (player1) {
+                            grid[0][0] = 1;
+                        } else {
+                            grid[0][0] = 2;
+                        }
+                    }
+
+                    if (mouseEvent.getY() >= 330 && mouseEvent.getY() <= 470 && grid[1][0] == 0) {
+                        positionX.add(460);
+                        positionY.add(400);
+                        if (player1) {
+                            grid[1][0] = 1;
+                        } else {
+                            grid[1][0] = 2;
+                        }
+                    }
+
+                    if (mouseEvent.getY() > 470 && grid[2][0] == 0) {
+                        positionX.add(460);
+                        positionY.add(540);
+                        if (player1) {
+                            grid[2][0] = 1;
+                        } else {
+                            grid[2][0] = 2;
+                        }
+                    }
+                }
+
+                if ((mouseEvent.getX() >= 530) && mouseEvent.getX() <= 670) {
+                    if (mouseEvent.getY() < 330 && grid[0][1] == 0) {
+                        positionX.add(600);
+                        positionY.add(260);
+                        if (player1) {
+                            grid[0][1] = 1;
+                        } else {
+                            grid[0][1] = 2;
+                        }
+                    }
+
+                    if (mouseEvent.getY() >= 330 && mouseEvent.getY() <= 470 && grid[1][1] == 0) {
+                        positionX.add(600);
+                        positionY.add(400);
+                        if (player1) {
+                            grid[1][1] = 1;
+                        } else {
+                            grid[1][1] = 2;
+                        }
+                    }
+
+                    if (mouseEvent.getY() > 470 && grid[2][1] == 0) {
+                        positionX.add(600);
+                        positionY.add(540);
+                        if (player1) {
+                            grid[2][1] = 1;
+                        } else {
+                            grid[2][1] = 2;
+                        }
+                    }
+                }
+
+                if (mouseEvent.getX() >= 670) {
+                    if (mouseEvent.getY() < 330 && grid[0][2] == 0) {
+                        positionX.add(740);
+                        positionY.add(260);
+                        if (player1) {
+                            grid[0][2] = 1;
+                        } else {
+                            grid[0][2] = 2;
+                        }
+                    }
+
+                    if (mouseEvent.getY() >= 330 && mouseEvent.getY() <= 470 && grid[1][2] == 0) {
+                        positionX.add(740);
+                        positionY.add(400);
+                        if (player1) {
+                            grid[1][2] = 1;
+                        } else {
+                            grid[1][2] = 2;
+                        }
+                    }
+
+                    if (mouseEvent.getY() > 470 && grid[2][2] == 0) {
+                        positionX.add(740);
+                        positionY.add(540);
+                        if (player1) {
+                            grid[2][2] = 1;
+                        } else {
+                            grid[2][2] = 2;
+                        }
+                    }
+                }
+
+                g.setColor(Color.green);
+                for (int i = 0; i < MouseController.getPositionX().size(); i++) {
+                    if (player1 == true) {
+                        g.drawLine(positionX.get(i) - 30, positionY.get(i) - 30, positionX.get(i) + 30, positionY.get(i) +30);
+                        g.drawLine(positionX.get(i) + 30, positionY.get(i) - 30, positionX.get(i) - 30, positionY.get(i) +30);
+
+                        player1 = false;
+                    } else {
+                        g.drawOval(positionX.get(i) - 30, positionY.get(i) - 30, 60, 60);
+
+                        player1 = true;
+                    }
+                }
+
+                if (grid[0][0] == 1 && grid[0][1] == 1 && grid[0][2] == 1 ||
+                        grid[1][0] == 1 && grid[1][1] == 1 && grid[1][2] == 1 ||
+                        grid[2][0] == 1 && grid[2][1] == 1 && grid[2][2] == 1 ||
+
+                        grid[0][0] == 1 && grid[1][0] == 1 && grid[2][0] == 1 ||
+                        grid[0][1] == 1 && grid[1][1] == 1 && grid[2][1] == 1 ||
+                        grid[0][2] == 1 && grid[1][2] == 1 && grid[2][2] == 1 ||
+
+                        grid[0][0] == 1 && grid[1][1] == 1 && grid[2][2] == 1 ||
+                        grid[2][0] == 1 && grid[1][1] == 1 && grid[0][2] == 1) {
+                    System.out.println("Player 1 Win");
+                }
+
+                if (grid[0][0] == 2 && grid[0][1] == 2 && grid[0][2] == 2 ||
+                        grid[1][0] == 2 && grid[1][1] == 2 && grid[1][2] == 2 ||
+                        grid[2][0] == 2 && grid[2][1] == 2 && grid[2][2] == 2 ||
+
+                        grid[0][0] == 2 && grid[1][0] == 2 && grid[2][0] == 2 ||
+                        grid[0][1] == 2 && grid[1][1] == 2 && grid[2][1] == 2 ||
+                        grid[0][2] == 2 && grid[1][2] == 2 && grid[2][2] == 2 ||
+
+                        grid[0][0] == 2 && grid[1][1] == 2 && grid[2][2] == 2 ||
+                        grid[2][0] == 2 && grid[1][1] == 2 && grid[0][2] == 2) {
+                    System.out.println("Player 2 Win");
+                }
             }
         } );
     }
 
-    /*@Override
+    @Override
     public void mousePressed(MouseEvent mouseEvent) {
 
-    }*/
+    }
 }
